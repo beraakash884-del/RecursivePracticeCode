9e438246d8cb9d4cfd5fe3f98c93b2e0f422199b
beraakash884-del <beraakash884@gmail.com>
2026-05-24 00:29:15 +0530

    improved code without useing global variable

--- PrintName.c
+++ PrintName.c
@@ -1,22 +1,21 @@
 // print name n times using recursion
 
 #include<stdio.h>
-void MyName (int);
-int count=0;
-int N ;
+void MyName (int, int);
+
 int main()
 {
+  int N;
  printf("Enter the number of times you want to print my name\n ");
  scanf("%d", &N);
-  MyName(N);
+  MyName(1,N);
 return 0;
 }
 
-void MyName (int n)
+void MyName (int i,int n)
 { 
-    if(count==n) 
+    if(i>n)   // base condition
       return ;
     printf("Akash Bera\n");
-   count++;
-    MyName(N);
+    MyName(i+1, n);
 }
