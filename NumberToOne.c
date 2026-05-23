aa2c76129ecf0a7b1abd7c1d3a55a5aef4457220
beraakash884-del <beraakash884@gmail.com>
2026-05-24 01:02:00 +0530

    improved vertion without useing global veriable

--- NumberToOne.c
+++ NumberToOne.c
@@ -1,26 +1,22 @@
 // This code  will print N to 1 number with recursion.
 
-#include<stdio.h>
+#include <stdio.h>
 void NumberToOne(int);
-int count;
-int number;
+
 int main()
 {
-printf("\n Enter the number \n");
-scanf("%d",&number);
+   int number;
+  printf("\n Enter the number \n");
+  scanf("%d", &number);
   NumberToOne(number);
-return 0;
+  return 0;
 }
 
-void NumberToOne (int n)
+void NumberToOne(int n)
 {
-    count=n;
- if(count==1)
- {
-    printf("%d ",count);   // this will print the one .
-     return;
- }
-     printf("%d ",count);
-     count--;
-     NumberToOne(count);
+  if (n == 0) // base condition
+    return;
+  printf("%d ", n);
+  // recursive code
+  NumberToOne(n-1);
 }
